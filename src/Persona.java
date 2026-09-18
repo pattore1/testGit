e8ff434a1b871cbd35a4e98fd4d2073583de6ea5
pattore <andrea.pastore@edu.elis.org>
2026-09-18 09:25:37 +0200

    messo classe Persona

---
+++ src/Persona.java
@@ -0,0 +1,13 @@
+public class Persona {
+    private String nome;
+    private String cognome;
+    private String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
