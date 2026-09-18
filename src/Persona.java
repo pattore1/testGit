681ebd6ba3d6271e2c7ad89e5fdde3338064451e
pattore <andrea.pastore@edu.elis.org>
2026-09-18 09:46:43 +0200

    Aggiunti getter setter

--- src/Persona.java
+++ src/Persona.java
@@ -10,4 +10,20 @@ public class Persona {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
